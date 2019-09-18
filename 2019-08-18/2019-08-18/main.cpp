df7b94d4652200a82d1aa3c7e4cbedb8053d8722
capalotmingo <55443390+capalotmingo@users.noreply.github.com>
2019-09-18 08:24:52 -0400

    Add files via upload

---
+++ 2019-08-18/2019-08-18/main.cpp
@@ -0,0 +1,12 @@
+//Laneef Mingo
+//Intro to Computer Science
+//Period 1
+//Mr.Williams
+//2019-09-18-Classwork
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Yo whats the word" << std::endl;
+}
