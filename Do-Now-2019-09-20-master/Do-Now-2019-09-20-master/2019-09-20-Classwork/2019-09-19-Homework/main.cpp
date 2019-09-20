ce522573d7f3d8d303015adef36d66863ba19dbd
capalotmingo <55443390+capalotmingo@users.noreply.github.com>
2019-09-20 08:27:06 -0400

    Add files via upload

---
+++ Do-Now-2019-09-20-master/Do-Now-2019-09-20-master/2019-09-20-Classwork/2019-09-19-Homework/main.cpp
@@ -0,0 +1,26 @@
+// Name: Laneef Mingo
+// Intro to CS
+// Period 1
+// Mr. Williams
+// 2019-09-20-Classwork
+
+// Assignment:	Format the code below so that it looks neat.
+//				Use the "next line" style for curly brackets.
+
+#include <iostream>
+
+int main()
+{
+	std::cout	<< "Name: Laneef Mingo" << std::endl 
+				<< "Class Schedule" << std::endl
+				<< "Period 1: Intro to CS" <<std::endl
+				<< "Period 2: Health" << std::endl
+				<< "Period 3: PE"<< std::endl
+				<< "Period 4: House" <<std::endl
+				<< "Period 5: Spanish" << std::endl
+				<< "Period 6: Lunch" << std::endl
+				<< "Period 7: English" << std::endl
+				<< "Period 8: Global" << std::endl
+				<< "Period 9: Geometry" << std::endl
+				<< "Period 10: Earth Science";
+}
