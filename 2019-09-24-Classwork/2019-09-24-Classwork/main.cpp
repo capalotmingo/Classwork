b48209c16eb769733408200a0b26f0e68eaf02e9
capalotmingo <55443390+capalotmingo@users.noreply.github.com>
2019-09-24 08:45:32 -0400

    Add files via upload

---
+++ 2019-09-24-Classwork/2019-09-24-Classwork/main.cpp
@@ -0,0 +1,15 @@
+//Name:Laneef Mingo
+//Intro To Comp Sci
+//Mr.Williams
+//Period 1
+//2019-09-24-Classwork
+
+#include<iostream>
+
+int main()
+{
+	int x = 10000;
+	
+
+	std::cout << x << std::endl;
+}
