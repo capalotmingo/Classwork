86c1afc72fa6a906bfd48ac24e3685b89d9db8bf
capalotmingo <55443390+capalotmingo@users.noreply.github.com>
2019-10-03 08:44:02 -0400

    Add files via upload

---
+++ 2019-10-3-Classwork/2019-10-3-Classwork/main.cpp
@@ -0,0 +1,15 @@
+//Name:Laneef Mingo
+//Intro to CS
+// Period 1
+// Mr. Williams
+// 2019-10-03-Classwork
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter your favorite";
+	int favoritenumber;
+	cin >> favoritenumber;
+	cout << "cool, " << favoritenumber << "is my favorite number too!" << endl;
+}
