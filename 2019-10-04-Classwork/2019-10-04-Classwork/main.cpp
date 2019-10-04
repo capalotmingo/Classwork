371d0ff2a1a69a9f49916e13e616423d26298ab7
capalotmingo <55443390+capalotmingo@users.noreply.github.com>
2019-10-04 08:45:52 -0400

    Add files via upload

---
+++ 2019-10-04-Classwork/2019-10-04-Classwork/main.cpp
@@ -0,0 +1,16 @@
+//Name:Laneef Mingo
+//Intro To CS
+//Period 1
+//Mr.Williams
+//2019-10-04-Classwork
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float p = 2 * 5.5 * 3.14;
+	float a = 5.5 * 5.5 * 3.14;
+	cout << p << endl << a;
+
+}   float p2=(2*4.5)+(2*7.9)
